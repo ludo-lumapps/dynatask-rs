47da19aa4030e3c068a31cd87accda9017ebf417
Ludovic VAUGEOIS PEPIN <ludovic.vaugeois@lumapps.com>
2025-11-24 09:50:05 +0000

    Make tests work in devcontainer

--- .devcontainer/post_install.sh
+++ .devcontainer/post_install.sh
@@ -4,4 +4,5 @@ echo "alias ccc='clear && cargo clippy'" >> ~/.bashrc
 echo "alias ccu='clear && cargo update'" >> ~/.bashrc
 echo "alias cct='clear && cargo test'" >> ~/.bashrc
 
-wget -qO - https://sh.rustup.rs | sh -s -- -y -q
+sudo service valkey-server start
+
