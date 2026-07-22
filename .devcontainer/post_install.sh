5fe6366398aae9704d30d63f7d0c615047d0c6e9
Ludovic VAUGEOIS PEPIN <ludovic.vaugeois@lumapps.com>
2026-07-22 07:10:59 +0000

    Add cargo edit to workspace

--- .devcontainer/post_install.sh
+++ .devcontainer/post_install.sh
@@ -4,4 +4,7 @@ echo "alias ccc='clear && cargo clippy'" >> ~/.bashrc
 echo "alias ccu='clear && cargo update'" >> ~/.bashrc
 echo "alias cct='clear && cargo test'" >> ~/.bashrc
 
-# cargo install cargo-edit
+# cargo upgrade
+curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+chmod +x /home/vscode/.cargo/bin/cargo-binstall
+/home/vscode/.cargo/bin/cargo-binstall cargo-edit -y
